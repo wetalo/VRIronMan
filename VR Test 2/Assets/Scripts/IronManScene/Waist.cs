d493e4f62e7d126386d650013c049c360fe08e79
Daniel Swieca <danielswieca@gmail.com>
2019-01-15 16:42:06 -0500

    Added guns that shoot and also spawning guns from waist.

--- VR Test 2/Assets/Scripts/IronManScene/Waist.cs
+++ VR Test 2/Assets/Scripts/IronManScene/Waist.cs
@@ -6,6 +6,8 @@ public class Waist : MonoBehaviour {
 
     public bool isTouchingController;
     public GameObject controllerTouched;
+    OVRGrabber grabber;
+    public GameObject gunPrefab;
 
     // Use this for initialization
     void Start()
@@ -16,22 +18,44 @@ public class Waist : MonoBehaviour {
     // Update is called once per frame
     void Update()
     {
+        if (isTouchingController)
+        {
+            bool triggerPulled = false;
+            if (grabber.name == "AvatarGrabberLeft")
+            {
+                triggerPulled = TouchHandler.TH.leftTouchPrimaryHandTriggerPulled;
+            }
+            else if (grabber.name == "AvatarGrabberRight")
+            {
+                triggerPulled = TouchHandler.TH.rightTouchPrimaryHandTriggerPulled;
+            }
 
+            if (triggerPulled && !grabber.grabbedObject)
+            {
+                GameObject gunInstance = GameObject.Instantiate(gunPrefab, controllerTouched.transform.position, controllerTouched.transform.rotation);
+                OVRGrabbable grabbable = gunInstance.GetComponent<OVRGrabbable>();
+                //grabbable.GrabBegin(grabber,grabbable.grabPoints[0]);
+                isTouchingController = false;
+            }
+        }
     }
 
     private void OnTriggerEnter(Collider other)
     {
-        if (other.name == "GrabVolumeSmall")
+        DebugText.DT.Debug(other.name + " entered");
+        if (other.name == "GrabVolumeSmall" || other.name == "GrabVolumeBig")
         {
             isTouchingController = true;
             controllerTouched = other.gameObject;
-            DebugText.DT.Debug(gameObject.name + " touched");
+
+            grabber = other.transform.parent.parent.gameObject.GetComponent<OVRGrabber>();
         }
     }
 
     private void OnTriggerExit(Collider other)
     {
-        if (other.name == "GrabVolumeSmall")
+        DebugText.DT.Debug(other.name + " exited");
+        if (other.name == "GrabVolumeSmall" || other.name == "GrabVolumeBig")
         {
             isTouchingController = false;
         }
