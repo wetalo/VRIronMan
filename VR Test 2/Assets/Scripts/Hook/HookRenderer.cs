fff8d026c51e9016abb74c9afdcc6fbab6732119
Daniel Swieca <danielswieca@gmail.com>
2019-01-24 12:29:04 -0500

    Hook

---
+++ VR Test 2/Assets/Scripts/Hook/HookRenderer.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HookRenderer : MonoBehaviour {
+
+    public FloatVariable maxDistance;
+
+    public GameObject targetPrefab;
+
+    GameObject targetPrefabInstance;
+    TargetPoint targetPoint;
+
+    Vector3 targetPosition;
+
+	// Use this for initialization
+	void Start () {
+        targetPrefabInstance = GameObject.Instantiate(targetPrefab);
+        targetPoint = targetPrefabInstance.GetComponent<TargetPoint>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        CastOutToTarget();
+
+    }
+
+    void CastOutToTarget()
+    {
+        int layer_mask = LayerMask.GetMask("Default");
+        RaycastHit hit;
+        Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * maxDistance.Value, Color.red);
+        if (Physics.Raycast(transform.position, transform.forward, out hit, maxDistance.Value, layer_mask))
+        {
+            targetPosition = hit.point;
+            //The object the projectile hit
+            targetPoint.SetValid(true);
+        }
+        else
+        {
+            targetPosition = transform.position + transform.forward * maxDistance.Value;
+            targetPoint.SetValid(false);
+        }
+
+        targetPoint.SetPosition(targetPosition);
+    }
+}
---
+++ VR Test 2/Assets/Scripts/Hook/TargetPoint.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TargetPoint : MonoBehaviour {
+
+    public Material targetPointValid;
+    public Material targetPointInvalid;
+
+    public Vector3 point;
+    public bool valid;
+
+    Renderer myRenderer;
+
+	// Use this for initialization
+	void Start () {
+        myRenderer = GetComponent<Renderer>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.position = point;
+
+    }
+
+    public void SetValid(bool valid)
+    {
+        this.valid = valid;
+
+        
+        if (valid)
+        {
+            myRenderer.material = targetPointValid;
+        } else
+        {
+            myRenderer.material = targetPointInvalid;
+        } 
+    }
+
+    public void SetPosition(Vector3 position)
+    {
+        this.point = position;
+    }
+}
